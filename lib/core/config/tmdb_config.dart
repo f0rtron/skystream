a2b4ad36130860620ab0d15df0b8d155fe1dc8ae
Akash Dayanand <akashdh11@gmail.com>
2026-04-19 22:50:19 -0400

    search optimizations

--- lib/core/config/tmdb_config.dart
+++ lib/core/config/tmdb_config.dart
@@ -3,7 +3,7 @@ class TmdbConfig {
   /// Pass via: flutter run --dart-define=TMDB_API_KEY=your_key_here
   static const String apiKey = String.fromEnvironment('TMDB_API_KEY');
   static const String baseUrl = 'https://api.themoviedb.org/3';
-  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/original';
+  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
   static const String posterSizeUrl = 'https://image.tmdb.org/t/p/w500';
   static const String backdropSizeUrl = 'https://image.tmdb.org/t/p/w1280';
   static const String profileSizeUrl = 'https://image.tmdb.org/t/p/w185';
